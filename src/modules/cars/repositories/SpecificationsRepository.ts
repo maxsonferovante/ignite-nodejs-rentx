e10a98ac536d3dca5c187c0787056863ab446c46
Maxson Ferovante <maxsonferovante@users.noreply.github.com>
2023-09-18 18:42:19 -0300

    singleton pattern implementation = garantir que a classe só tenha uma instância SpecifiticansRepository

--- src/modules/cars/repositories/SpecificationsRepository.ts
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -7,10 +7,18 @@ import { ISpecificationsRepository, ICreateSpecificationDTO } from "./ISpecifica
 export class SpecificationsRepository implements ISpecificationsRepository {
     private specifications: Specification[];
 
-    constructor() {
+    private static INSTANCE: SpecificationsRepository;
+
+    private constructor() {
         this.specifications = [];
     }
 
+    public static getInstance(): SpecificationsRepository {
+        if (!SpecificationsRepository.INSTANCE) {
+            SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+        }
+        return SpecificationsRepository.INSTANCE;
+    }
     create(data: ICreateSpecificationDTO): void {
         const specification = new Specification(data);
         this.specifications.push(specification);
